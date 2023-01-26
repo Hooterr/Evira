65df9bc49f73e27a787b881687a37224ca0c872a
Maksymilian Lach <maksymilian.lach99@gmail.com>
2023-01-26 20:56:11 +0100

    iOS selected icons

--- src/EviraApp/Pages/Debug/ControlGalleryPage.xaml.cs
+++ src/EviraApp/Pages/Debug/ControlGalleryPage.xaml.cs
@@ -8,6 +8,7 @@ public partial class ControlGalleryPage : BaseContentPage<ControlGalleryPageMode
 	public ControlGalleryPage(ControlGalleryPageModel pageModel) : base(pageModel)
 	{
 		InitializeComponent();
+		/*
 		var dict = Application.Current!.Resources.MergedDictionaries.ElementAt(1);
 
 		var keys = dict.Keys.ToList();
@@ -23,6 +24,7 @@ public partial class ControlGalleryPage : BaseContentPage<ControlGalleryPageMode
 				});
 			}
 		}
+		*/
 
 	}
 }
--- src/EviraApp/Platforms/iOS/MyShellRenderer.cs
+++ src/EviraApp/Platforms/iOS/MyShellRenderer.cs
@@ -1,22 +1,20 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Platform.Compatibility;
 using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
 using UIKit;
+
 namespace Evira.App.Platforms;
 
 public class MyShellRenderer : ShellRenderer
 {
     protected override IShellSectionRenderer CreateShellSectionRenderer(ShellSection shellSection)
     {
-        var renderer = base.CreateShellSectionRenderer(shellSection);
-        if (renderer != null)
-        {
-            
-        }
-        return renderer;
+        return new MyShellSectionRenderer(this);
     }
 
     protected override IShellTabBarAppearanceTracker CreateTabBarAppearanceTracker()
@@ -26,16 +24,56 @@ public class MyShellRenderer : ShellRenderer
 }
 
 
+public class MyShellSectionRenderer : ShellSectionRenderer
+{
+    private static readonly Lazy<Func<ShellSection, bool, IMauiContext?>> _lazyFindMauiContext = new Lazy<Func<ShellSection, bool, IMauiContext?>>(CreateDelegate);
+
+    private static Func<ShellSection, bool, IMauiContext?> CreateDelegate()
+    {
+        var methodInfo = typeof(Microsoft.Maui.Controls.ViewExtensions).GetMethod("FindMauiContext",
+            BindingFlags.Static | BindingFlags.NonPublic);
+
+        ArgumentNullException.ThrowIfNull(methodInfo);
+
+        return (Func<ShellSection, bool, IMauiContext?>)System.Delegate.CreateDelegate(typeof(Func<ShellSection, bool, IMauiContext?>), methodInfo);
+    }
+
+    private static Func<ShellSection, bool, IMauiContext?> FindMauiContext => _lazyFindMauiContext.Value;
+
+    public MyShellSectionRenderer(IShellContext context) : base(context)
+    {
+    }
+
+    protected override void UpdateTabBarItem()
+    {
+        Title = ShellSection.Title;
+
+        var context = FindMauiContext(ShellSection, false);
+            
+        ShellSection.Icon.LoadImage(context!, icon =>
+        {
+            TabBarItem = new UITabBarItem(ShellSection.Title, icon?.Value, null);
+            TabBarItem.AccessibilityIdentifier = ShellSection.AutomationId ?? ShellSection.Title;
+
+            var selectedImageSource = AttachedProperties.SelectedIconProperty.GetSource(ShellSection.CurrentItem);
+            if (selectedImageSource == null)
+            {
+                return;
+            }
+
+            selectedImageSource.LoadImage(context!, icon =>
+            {
+                TabBarItem.SelectedImage = icon!.Value;
+            });
+        });
+    }
+}
+
 public class MyShellTabBarAppearanceTracker : SafeShellTabBarAppearanceTracker
 {
     public override void SetAppearance(UITabBarController controller, ShellAppearance appearance)
     {
-        if (OperatingSystem.IsIOSVersionAtLeast(15, 0))
-        {
-            var uiAppearance = new UITabBarAppearance();
-            uiAppearance.ConfigureWithOpaqueBackground();
-            typeof(SafeShellTabBarAppearanceTracker).GetField("_tabBarAppearance", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(this, uiAppearance);
-        }
+        controller.TabBar.Translucent = false;
         base.SetAppearance(controller, appearance);
     }
 }

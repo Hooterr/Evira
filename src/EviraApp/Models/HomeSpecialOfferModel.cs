5cd34487aeb155be342e55556c298f8653db55d4
Maksymilian Lach <maks.lach99@gmail.com>
2023-01-27 13:38:04 +0100

    Home page

---
+++ src/EviraApp/Models/HomeSpecialOfferModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Evira.App.Models
+{
+    public class HomeSpecialOfferModel
+    {
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+    }
+}
--- src/EviraApp/PageModels/Home/HomePageModel.cs
+++ src/EviraApp/PageModels/Home/HomePageModel.cs
@@ -1,6 +1,7 @@
 using System;
 using Evira.App.PageModels.Abstract;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Evira.App.Models;
 
 namespace Evira.App.PageModels.Home;
 
@@ -12,6 +13,8 @@ public partial class HomePageModel : BasePageModel
     
     #region Public Properties
 
+    public List<HomeSpecialOfferModel> SpecialOffers { get; }
+
     #endregion
 
     #region Constructor
@@ -21,6 +24,37 @@ public partial class HomePageModel : BasePageModel
     /// </summary>
     public HomePageModel()
     {
+        SpecialOffers = new List<HomeSpecialOfferModel>
+        {
+            new HomeSpecialOfferModel
+            {
+                Title = "30%",
+                SubTitle = "Today's Special",
+                Content = "Get discount for every\r\norder, only valid for today",
+                Image = "banner1",
+            },
+            new HomeSpecialOfferModel
+            {
+                Title = "25%",
+                SubTitle = "Weekends Deals",
+                Content = "Get discount for every\r\norder, only valid for today",
+                Image = "banner2",
+            },
+            new HomeSpecialOfferModel
+            {
+                Title = "40%",
+                SubTitle = "New Arrivals",
+                Content = "Get discount for every\r\norder, only valid for today",
+                Image = "banner3",
+            },
+            new HomeSpecialOfferModel
+            {
+                Title = "20%",
+                SubTitle = "Black Friday",
+                Content = "Get discount for every\r\norder, only valid for today",
+                Image = "banner4",
+            },
+        };
     }
 
     #endregion

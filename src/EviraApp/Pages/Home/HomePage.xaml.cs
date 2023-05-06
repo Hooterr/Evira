a71c20c05942efb20fd13552f90bdfdacc91942c
Maksymilian Lach <maksymilian.lach99@gmail.com>
2023-05-06 21:36:02 +0200

    Fix broken iOS home page

--- src/EviraApp/Pages/Home/HomePage.xaml.cs
+++ src/EviraApp/Pages/Home/HomePage.xaml.cs
@@ -1,3 +1,4 @@
+using Evira.App.Enums;
 using Evira.App.PageModels.Home;
 using Evira.App.Pages.Abstract;
 
@@ -5,22 +6,26 @@ namespace Evira.App.Pages.Home;
 
 public partial class HomePage : BaseContentPage<HomePageModel>
 {
-	public HomePage(HomePageModel pageModel) : base(pageModel)
+    private readonly Thickness safeArea;
+
+    public HomePage(HomePageModel pageModel) : base(pageModel)
 	{
 		InitializeComponent();
-	}
+        safeArea = GetSafeAreaInsets();
+    }
 
     protected override void OnSizeAllocated(double width, double height)
     {
         base.OnSizeAllocated(width, height);
+#if ANDROID
         if (width == 0 || height == 0)
         {
             return;
         }
-
         contentSpacer.HeightRequest = searchView.Y + searchView.Height;
+#endif
     }
-
+    
     private void ScrollUpButtonTapped(object sender, TappedEventArgs e)
     {
         mainCollection.ScrollTo(0, animate: true);
@@ -29,8 +34,8 @@ public partial class HomePage : BaseContentPage<HomePageModel>
     private bool reachedThreshold;
     private void MainCollection_Scrolled(object sender, ItemsViewScrolledEventArgs e)
     {
-        headerView.TranslationY = -Math.Min(e.VerticalOffset, headerView.Y + headerView.Height);
-        searchView.TranslationY = -Math.Min(e.VerticalOffset, searchView.Y);
+        //headerView.TranslationY = -Math.Min(e.VerticalOffset, headerView.Y + headerView.Height - safeArea.Top);
+        searchView.TranslationY = -Math.Min(e.VerticalOffset, safeArea.Top + searchView.Padding.Top);
 
         if (!reachedThreshold && e.VerticalOffset > 30)
         {
@@ -44,5 +49,17 @@ public partial class HomePage : BaseContentPage<HomePageModel>
             scrollButton.FadeTo(0.0);
             reachedThreshold = false;
         }
+    }
+
+    private void MainGrid_OnSizeChanged(object sender, EventArgs e)
+    {
+#if IOS
+        contentSpacer.HeightRequest = searchView.Y + searchView.Height;
+
+        // Yikes...
+        var mi = typeof(VisualElement).GetMethod("InvalidateMeasure", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+        mi.Invoke(headerLayout, null);
+#endif
     }
 }
